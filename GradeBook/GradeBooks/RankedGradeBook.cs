b30d269cb6c78cb837423892f66fdff5f1cc33aa
Victor Ajani <ajavi1@qc.cgi.ca>
2019-04-01 15:55:51 -0400

    new GradeBookTypes update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,9 +4,9 @@ using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook // this inherits the BaseGradeBook class
     {
-        public RankedGradeBook( string name)
+        public RankedGradeBook(string name) : base(name) // create call to base constructor
         {
             Type = GradeBookType.Ranked;
         }
